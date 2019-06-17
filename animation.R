c4da0accb2da47670dcb9f6fd7c9fd73aab8c0a1
Paulj1989 <paul.robert.johnson@hotmail.com>
2019-06-17 12:52:49 -0500

    Playing with colors for animation & producing more data

--- animation.R
+++ animation.R
@@ -6,7 +6,7 @@ library(hrbrthemes)
 library(ineq)
 library(gganimate)
 library(gifski)
-library(transformr)
+library(ggthemes)
 
 time3 <- read_csv("~/Documents/GitHub Directory/ftw/time3.csv")
 big3$year <- as.factor(big3$year)
@@ -19,8 +19,8 @@ my.animation <- ggplot(time3, aes(value, pts, color = country)) +
   theme_ipsum(base_size = 25, axis_title_size = 25)+
   theme(legend.position="bottom",
         plot.margin = margin(5.5, 40, 5.5, 5.5))+
-  scale_color_brewer(name=NULL,
-                        labels=c("Bundesliga", "Premier League", "La Liga"), palette="Set1")+
+  scale_color_calc(name=NULL,
+                        labels=c("Bundesliga", "Premier League", "La Liga"))+
   labs(title = 'Year: {closest_state}', x = 'Market Value (€m)', y = 'Points') +
   transition_states(year, state_length = 0) +
   coord_cartesian(clip = 'off') + 
@@ -50,13 +50,33 @@ anim_save("value.gif")
 
 #Transfer spending animation
 
-ggplot(time3, aes(value, group = year)) + 
+spend <- read_csv("~/Documents/GitHub Directory/ftw/spend.csv")
+
+spend_plot <- spend %>%
+  ggplot(aes(Season, Spend, group=League, color=League)) +
+  geom_line(size = 1, alpha = 0.6)+
+  geom_point(size=4, alpha = 0.6)+xlab("Year")+ylab("Transfer Spending (€m)")+
+  theme_ipsum(base_size = 18, axis_title_size = 18)+
+  labs(color = "League")+
+  scale_color_brewer(name = NULL, palette = "Set1")+
+  theme(axis.text.x = element_text(angle=45))+
+  transition_states(Season, state_length = 0)
+
+
+animate(spend_plot, height = 800, width =800, nframes = 100, duration = 20)
+anim_save("value.gif")
+
+spend_plot
+
+spend_plot <- ggplot(spend, aes(Season, Spend, group=League, color = League)) + 
   geom_line() + 
-  geom_segment(aes(xend = 31, yend = Temp), linetype = 2, colour = 'grey') + 
+  geom_segment(aes(xend = Season, yend = Spend), linetype = 2, colour = 'grey') + 
   geom_point(size = 2) + 
-  geom_text(aes(x = 31.1, label = year), hjust = 0) + 
-  transition_reveal(year) + 
+  transition_reveal(Year) + 
   coord_cartesian(clip = 'off') + 
-  labs(title = 'Temperature in New York', y = 'Temperature (°F)') + 
-  theme_minimal() + 
-  theme(plot.margin = margin(5.5, 40, 5.5, 5.5))
+  labs(title = NULL, y = 'Transfer Spending (€m)') + 
+  scale_color_calc(name = NULL)+
+  theme_ipsum(base_size = 25, axis_title_size = 25)+
+  theme(legend.position="bottom", axis.text.x = element_text(angle=45))
+
+animate(spend_plot, height = 800, width =800, nframes = 100, duration = 20)
