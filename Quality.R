0a65e0beb2d3daaf5646a1426e6715f4f2640222
Paulj1989 <paul.robert.johnson@hotmail.com>
2019-07-06 12:09:45 -0500

    Finalizing analysis

--- Quality.R
+++ Quality.R
@@ -7,13 +7,15 @@ library(gganimate)
 
 ucl<- read_csv("~/Documents/GitHub Directory/ftw/ucl.csv")
 
+
+ggthemr('greyscale', layout = "scientific", text_size = 30)
+
 ucl %>%
   ggplot(aes(x = reorder(Season, Year), Pts, group=League, color=League)) +
-  geom_line(size = 0.8, alpha = 0.6)+
-  geom_point(size=2, alpha = 0.7)+ xlab("Season") + ylab("Champions League Performance")+
+  geom_line(size = 0.8, alpha = 1)+
+  geom_point(size=4, alpha = 1)+ xlab("Season") + ylab("Champions League Performance")+
   labs(color = "League")+
-  scale_color_calc(name = NULL)+
-  theme_ipsum(base_size = 25, axis_title_size = 25, grid = FALSE)+
-  theme(legend.position="bottom", axis.text.x = element_text(angle=45),
+  scale_color_lancet()+
+  theme(text = element_text(size=25), legend.position="bottom", axis.text.x = element_text(angle=45),
         plot.margin = margin(50, 50, 0, 50))
 
--- animation.R
+++ animation.R
@@ -2,8 +2,6 @@ rm(list=ls())
 library(foreign)
 library(tidyverse)
 library(ggpubr)
-library(hrbrthemes)
-library(ineq)
 library(gganimate)
 library(gifski)
 library(ggthemes)
@@ -11,18 +9,16 @@ library(ggrepel)
 library(directlabels)
 
 time3 <- read_csv("~/Documents/GitHub Directory/ftw/time3.csv")
-big3$year <- as.factor(big3$year)
 
+ggthemr('greyscale', layout = "scientific", text_size = 25)
 
-#Goals/Performance animation
+#Value/Points animation
 
-my.animation <- ggplot(time3, aes(value, pts, color = country)) +
+value_plot <- ggplot(time3, aes(value, pts, color = country)) +
   geom_point(alpha = 0.7, size = 12) +
-  theme_ipsum(base_size = 25, axis_title_size = 25)+
   theme(legend.position="bottom",
         plot.margin = margin(5.5, 40, 5.5, 5.5))+
-  scale_color_calc(name=NULL,
-                        labels=c("Bundesliga", "Premier League", "La Liga"))+
+  scale_color_lancet(labels=c("Bundesliga", "Premier League", "La Liga"))+
   labs(title = 'Year: {closest_state}', x = 'Market Value (€m)', y = 'Points') +
   transition_states(year, state_length = 0) +
   coord_cartesian(clip = 'off') + 
@@ -31,23 +27,6 @@ ease_aes('linear')
 
 
 animate(my.animation, height = 1200, width = 1000, nframes = 100, duration = 20)
-anim_save("value.gif")
-
-my.animation <- ggplot(time3, aes(value, color = country)) +
-  geom_density(alpha = 0.7, size = 12) +
-  theme_ipsum(base_size = 25, axis_title_size = 25)+
-  theme(legend.position="bottom",
-        plot.margin = margin(5.5, 40, 5.5, 5.5))+
-  scale_color_brewer(name=NULL,
-                     labels=c("Bundesliga", "Premier League", "La Liga"), palette="Set1")+
-  labs(title = 'Year: {closest_state}', x = 'Market Value (€m)', y = 'Points') +
-  transition_states(year, state_length = 0) +
-  coord_cartesian(clip = 'off') + 
-  shadow_mark(alpha = 0.3, size = 5)+
-  ease_aes('linear')
-
-
-animate(my.animation, height = 800, width =800, nframes = 100, duration = 20)
 anim_save("value.gif")
 
 #Transfer spending animation
@@ -63,22 +42,12 @@ spend_plot <- spend %>%
   transition_reveal(Year, keep_last = FALSE) + 
   coord_cartesian(clip = 'off') + 
   labs(title = NULL, y = 'Transfer Spending (€m)') + 
-  scale_color_calc(name = NULL)+
-  theme_ipsum(base_size = 25, axis_title_size = 25)+
+  scale_color_lancet()+
   theme(legend.position="none", axis.text.x = element_text(angle=45),
         plot.margin = margin(50, 200, 50, 50))
 
 
-animate(spend_plot, height = 1000, width = 1000, end_pause = 30)
+animate(spend_plot, height = 1000, width = 1000, end_pause = 5)
 anim_save("spend.gif")
 
-pts_all <- ggplot(big3)+
-  geom_density(aes(pts, fill = country), alpha=0.6)+
-  theme_ipsum(base_size = 18, axis_title_size = 18)+
-  labs(y = "", x = "Points")+
-  xlim(0,120)+
-  scale_fill_calc(name=NULL,
-                  labels=c("Bundesliga", "Premier League", "La Liga")) +
-  transition_reveal(year) + 
-  coord_cartesian(clip = 'off')
-pts_all
+write_csv(N, "asd.csv")
