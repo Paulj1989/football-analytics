2c104ab1cda647351415d3c102939d8fbdf63385
Paulj1989 <paulrjohnson1989@gmail.com>
2020-05-26 13:57:16 -0500

    Updating for .gitignore

---
+++ BuLi-Map/map.R
@@ -0,0 +1,26 @@
+library(shiny)
+library(ggmap)
+library(mapproj)
+library(rgdal)
+library(tidyverse)
+library(ggthemr)
+library(ggpubr)
+library(ggsci)
+library(rsconnect)
+
+germany <- readOGR(dsn = "~/Documents/GitHub Directory/ftw",
+                   layer = "Germanypolygon21")
+
+germany_data <- as_tibble(map_data(germany))
+
+ggthemr('greyscale', layout = "scientific", text_size = 25)
+
+ggplot(germany_data, aes(long, lat, group = group)) +
+  geom_polygon(color = "white") + theme(axis.line = element_blank(),
+                                        axis.ticks = element_blank(),
+                                        panel.grid = element_blank(),
+                                        axis.title = element_blank(),
+                                        axis.text = element_blank(),
+                                        legend.position = "right")
+
+
