4d43ba23aee1f1859f68bd2ac6f3a41a5b721a90
Paul Johnson <paul@paulrjohnson.net>
2021-02-18 22:43:09 -0600

    Update Sancho figure

--- sancho/sancho.R
+++ sancho/sancho.R
@@ -1,8 +1,7 @@
 pacman::p_load(tidyverse)
 
 # load data
-sancho <-
-  readr::read_csv("/home/paulj1989/Repositories/football-analytics/ftw/sancho/sancho.csv")
+sancho <- read_csv(here::here("sancho", "sancho.csv"))
 
 # create labels for y axis
 labels <- c(
@@ -27,7 +26,6 @@ sancho %>%
   mutate(
     # create plus/minus for defining segment color
     plus_minus = ifelse(change > 0, "plus", "minus"),
-    change = change * 100,
     # create id variable for ordering variables in plot
     id = cur_group_rows()
   ) %>%
@@ -39,36 +37,33 @@ sancho %>%
     y = reorder(stat, desc(id)),
     yend = reorder(stat, desc(id)),
     color = plus_minus
-  ), size = 15) +
+  ), size = 16.5) +
   # draw thicker line at 0 to represent starting/pivot point
   geom_vline(xintercept = 0, color = "grey20", size = 1.2) +
   # annotate figure to add title, subtitle and caption
-  annotate("text", 3.5, 14,
+  annotate("text", 0.035, 14,
            label = "Jadon Sancho's Offensive Struggles This Season",
-           family = "IBM Plex Sans", color = "black",
-           hjust = 0, size = 5.5, lineheight = 0.5) +
-  annotate("text", 3.5, 13.2,
+           family = "Montserrat", color = "grey10",
+           hjust = 0, size = 7, lineheight = 0.5) +
+  annotate("text", 0.035, 13.1,
            label = "Comparing the % Change in Jadon Sancho's\n/90 Offensive Statistics from 19/20 to 20/21",
-           family = "IBM Plex Sans", color = "black",
-           hjust = 0, size = 4, lineheight = 1.2) +
-  annotate("text", 3.5, 12.5,
-           label = "Source: FB Ref/StatsBomb | Graphic: @paul_johnson89",
-           family = "IBM Plex Sans", color = "black",
-           hjust = 0, size = 2.5, lineheight = 0.5) +
+           family = "Montserrat", color = "grey30",
+           hjust = 0, size = 5, lineheight = 1.2) +
+  annotate("text", 0.035, 12.3,
+           label = "Source: FB Ref/StatsBomb (01/21) | Graphic: @paul_johnson89",
+           family = "Montserrat", color = "grey30",
+           hjust = 0, size = 3.5, lineheight = 0.5) +
   # set scales for x and y
   scale_y_discrete(labels = rev(labels)) +
-  scale_x_continuous(expand = expansion(add = c(5, 10)),
-                     breaks = seq(-70, 70, 10),
-                     labels = seq(-70, 70, 10),
-                     limits = c(-70, 70)) +
+  scale_x_continuous(labels = function(x) scales::percent(x, accuracy = 1), breaks = seq(-0.5, 0.7, 0.1), limits = c(-0.55, 0.7)) +
   # set theme and customize to fit our requirements here
   theme_minimal(base_family = "Fira Code", base_size = 14) +
   scale_color_manual(values = c(plus = "#007F5F", minus ="#DE2141")) +
-  labs(x = "% Change") +
   theme(
+    plot.margin = margin(1, 1, 1, 1, unit = "cm"),
     legend.position = "none",
-    axis.text = element_text(size = 8),
-    axis.title.x = element_text(size = 10, color = "grey30"),
+    axis.text = element_text(size = 12),
+    axis.title.x = element_blank(),
     axis.title.y = element_blank(),
     panel.grid.major.x = element_line(color = "#F3F5F6", size = 0.4),
     panel.grid.minor.x = element_blank(),
@@ -76,4 +71,4 @@ sancho %>%
   )
 
 # save image
-ggsave(here::here("/home/paulj1989/Repositories/football-analytics/ftw/sancho/sancho.png"), dpi = 320, width = 16, height = 9)
+ggsave(here::here("sancho", "sancho.png"), dpi = 320, width = 16, height = 9)
